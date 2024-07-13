165fd30b63bb861ae236e126a40eb4f517d9bab5
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2024-07-13 00:23:28 -0400

    added some TODOs for apps

--- website/home.js
+++ website/home.js
@@ -44,6 +44,10 @@ function loadApps(){
         const devLogList = document.querySelector('.featured-items-container');
         for (let index = 0; index < maxIndex; index++) {
             let app = data.apps[index];
+
+            //TODO: figure this out.
+            //probably need to add a link to the app page
+            // or should we just link to the actualy store page?
             // const link = document.createElement('a');
             // link.href = "#";
             // link.onclick = () => {
@@ -55,10 +59,10 @@ function loadApps(){
 
             const featuredItemDiv = document.createElement('div');
             featuredItemDiv.classList.add('featured-item');
-        
+
             // Create the image div
             const imageDiv = document.createElement('div');
-            imageDiv.textContent =  "image"; //imageText;
+            imageDiv.textContent =  "image"; //imageText; //TODO: needs to be the featured image of the app.
         
             // Create the h4 element
             const titleH4 = document.createElement('h4');
